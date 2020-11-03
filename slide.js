e60a2911aa677515b178a694f96a1999b0aedb32
xiix1205 <68121134+xiix1205@users.noreply.github.com>
2020-11-04 01:15:33 +0900

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,122 @@
+const all = ele => document.querySelectorAll(ele)
+const one = ele => document.querySelector(ele)
+const slide = _ => {
+  const wrap = one('.slide')
+  const target = wrap.children[0]
+  const len = target.children.length
+  const liStyle = `
+    position:absolute;
+    left:0;right:0;top:0;bottom:0;transition:1s;opacity:0
+  `
+  target.style.cssText = `position:relative;`
+  Array.from(target.children).forEach(ele => ele.style.cssText = liStyle)
+  target.children[0].style.opacity = 1
+  let pos = 0
+  setInterval(_ => {
+    target.children[pos].style.opacity = 0
+    pos = (pos + 1) % len 
+    target.children[pos].style.opacity = 1
+  }, 3500)
+}
+window.onload = function () {
+  slide()
+}
+
+function main(){
+    window.location = "main.html"
+}
+
+function login(){
+    window.location= "https://kana9648.github.io/Movie/login.html"
+}
+
+function join(){
+    window.location = "https://kana9648.github.io/Movie/in.html"
+}
+
+function board_1(){
+    window.location = "http://haha922.dothome.co.kr/movie_html.html"
+}
+
+function board_2(){
+    window.location = "http://ieouy0605.dothome.co.kr/subpage/board.html#"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
