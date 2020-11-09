8f91b8e2006c3189814a90e1e68f212d6d90384a
xiix1205 <68121134+xiix1205@users.noreply.github.com>
2020-11-09 14:29:54 +0900

    Add files via upload

--- slide.js
+++ slide.js
@@ -39,8 +39,100 @@ function board_1(){
 }
 
 function board_2(){
-    window.location = "http://ieouy0605.dothome.co.kr/subpage/board.html#"
+    window.location = "http://ieouy0605.dothome.co.kr/subpage/subpage/board.html"
 }
+
+function board_3(){
+    window.location ="https://kana9648.github.io/Movie/read.html"
+}
+
+function board_4(){
+    window.location = "http://kimhappy93.dothome.co.kr/movie_detail.html"
+}
+
+function seoul(){
+   
+}
+
+/*      
+
+
+*/
+
+
+//var slideIndex = 1;
+//showDivs(slideIndex);
+//
+//function plusDivs(n) {
+//  showDivs(slideIndex += n);
+//}
+//
+//function showDivs(n) {
+//  var i;
+//  var x = document.getElementsByClassName("post_view");
+//  if (n > x.length) {slideIndex = 1}
+//  if (n < 1) {slideIndex = x.length}
+//  for (i = 0; i < x.length; i++) {
+//    x[i].style.display = "none";  
+//  }
+//  x[slideIndex-1].style.display = "block";  
+//}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("post_view");
+  if (n > slides.length){
+      slideIndex = 1
+  }    
+  if (n < 1){
+      slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
